13812328c923c976882698ffdf2ec8de212a430a
Jordan Buckmaster <buckmast@purdue.edu>
2018-02-10 19:21:17 -0500

    Create basic authentication flow and persist auth state

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,15 +1,57 @@
-import React, { Component } from 'react';
-import '../css/Login.css';
+import React, { Component } from 'react'
+
+import '../css/Login.css'
+import { auth } from '../firebase'
 
 class Login extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = { 
+      error: '',
+    }
+  }
+
+  handleSubmit = (ev) => {
+    ev.preventDefault()
+   
+    const email = ev.target.email.value
+    const password = ev.target.password.value
+
+    auth.logInWithEmailAndPassword(email, password)
+      .catch(error => {
+        this.setState({ error })
+      });
+  }
+
 
   render() {
     return (
       <div className="Login">
-       
+          <form onSubmit={this.handleSubmit}>
+          <input
+            required
+            name="email"
+            type="text"
+            placeholder="Email Address"
+            />
+          <input
+            required
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
+          <button type="submit">
+            Log In
+          </button>
+
+          { this.state.error && <p>{this.state.error.message}</p> }
+        </form>
       </div>
     );
   }
 }
 
 export default Login;
+
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,20 @@
-import React, { Component } from 'react';
-import '../css/Navbar.css';
+import React, { Component } from 'react'
+
+import '../css/Navbar.css'
+import { auth } from '../firebase'
 
 class Navbar extends Component {
+
 
   render() {
     return (
       <div className="Navbar">
-     
+        <button
+          type="button"
+          onClick={auth.logOut}
+        >
+          Log Out
+        </button>
       </div>
     );
   }
