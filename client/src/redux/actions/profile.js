1d533fc663eac4f67d3bd834a71768c943315be3
PaulkaToast <toth21@purdue.edu>
2018-02-23 07:19:39 -0500

    Make API request code more reusable

--- client/src/redux/actions/profile.js
+++ client/src/redux/actions/profile.js
@@ -1,3 +1,25 @@
+function genericDispatch(hasError, isLoading, success) {
+    function specificDispatch(url, headers) {
+        return (dispatch) => {
+            dispatch(isLoading(true))
+            fetch(url, { headers, method: 'GET', mode: 'cors'})
+                .then((response) => {
+                    if (!response.ok) {
+                        throw Error(response.statusText);
+                    }
+                    dispatch(isLoading(false));
+                    return response;
+                })
+                .then((response) => response.json())
+                .then((data) => {
+                    dispatch(success(data))
+                })
+                .catch(() => dispatch(hasError(true)));
+        }
+    }
+    return specificDispatch;
+}
+
 export function getProfileHasError(hasError) {
     return {
         type: 'GET_PROFILE_HAS_ERROR',
@@ -19,21 +41,31 @@ export function getProfileDataSuccess(data) {
     }
 }
 
-export function getProfile(url, headers) {
-    return (dispatch) => {
-        dispatch(getProfileIsLoading(true))
-        fetch(url, { headers, method: 'GET', mode: 'cors'})
-            .then((response) => {
-                if (!response.ok) {
-                    throw Error(response.statusText);
-                }
-                dispatch(getProfileIsLoading(false));
-                return response;
-            })
-            .then((response) => response.json())
-            .then((data) => {
-                dispatch(getProfileDataSuccess(data))
-            })
-            .catch(() => dispatch(getProfileHasError(true)));
+export const getProfile = genericDispatch(
+    getProfileHasError, getProfileIsLoading, getProfileDataSuccess
+)
+
+export function deleteProfileHasError(hasError) {
+    return {
+        type: 'DELETE_PROFILE_HAS_ERROR',
+        hasError
+    }  
+}
+
+export function deleteProfileIsLoading(isLoading) {
+    return {
+        type: 'DELETE_PROFILE_IS_LOADING',
+        isLoading
     }
 }
+
+export function deleteProfileDataSuccess(data) {
+    return {
+        type: 'DELETE_PROFILE_DATA_SUCCESS',
+        data
+    }
+}
+
+export const deleteProfile = genericDispatch(
+    deleteProfileHasError, deleteProfileIsLoading, deleteProfileDataSuccess
+)
