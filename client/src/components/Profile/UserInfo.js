b2bff2edaf03a577629926ac63f83b7f203c2dc2
Kevin <kevinnagar29@gmail.com>
2018-02-23 13:49:43 -0500

    Added a basic profile template

--- client/src/components/Profile/UserInfo.js
+++ client/src/components/Profile/UserInfo.js
@@ -5,8 +5,12 @@ class UserInfo extends Component {
   render() {
     return (
       <div className="UserInfo">
-      
-      </div>
+           <img src="avatar_hat.jpg" alt="Avatar"></img>
+             <div class="avatar">
+                <h2>Jane Doe</h2>
+             </div>
+
+      </div>  
     )
   }
 }
