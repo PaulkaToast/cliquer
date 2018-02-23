e34e383d1adc17a95572b0ae7f08957e64c0ddee
PaulkaToast <toth21@purdue.edu>
2018-02-23 04:07:56 -0500

    revert to Jordan's commit

--- client/src/react-compiled.js
+++
@@ -1,9 +0,0 @@
-
-function SvgMapPin(props) {
-  return React.createElement(
-    'svg',
-    { xmlns: 'http://www.w3.org/2000/svg', width: '24', height: '24', viewBox: '0 0 24 24', fill: 'none', stroke: '#424242', strokeWidth: '2', strokeLinecap: 'round', strokeLinejoin: 'round' },
-    React.createElement('path', { d: 'M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z' }),
-    React.createElement('circle', { cx: '12', cy: '10', r: '3' })
-  );
-}
