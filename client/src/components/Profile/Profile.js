c5c1bfcfb65d5c47fb32f752979b9130f7ae3650
Kevin <kevinnagar29@gmail.com>
2018-03-08 10:50:53 -0500

    Implemented more changed to the profile page

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -11,93 +11,85 @@ import { getSkills, removeSkill, getProfile, getSkillsList } from '../../redux/a
 class Profile extends Component {
 
   componentWillReceiveProps = (nextProps) => {
-    if(nextProps.uid && nextProps.token) {
-      this.props.getSkills(`https://localhost:17922/api/getSkills?username=${nextProps.uid}`, { 'X-Authorization-Firebase': nextProps.token})
-     
+    if (nextProps.uid && nextProps.token) {
+      this.props.getSkills(`https://localhost:17922/api/getSkills?username=${nextProps.uid}`, { 'X-Authorization-Firebase': nextProps.token })
+
       const type = nextProps.ownerUID === nextProps.uid ? 'user' : 'public'
       const uid = nextProps.uid //TODO: remove this later and just use ownerid
-      this.props.getSkillsList('https://localhost:17922/api/getSkillList', { 'X-Authorization-Firebase': nextProps.token})
-      this.props.getProfile(`https://localhost:17922/api/getProfile?identifier=${uid}&type=${type}`, { 'X-Authorization-Firebase': nextProps.token})
+      this.props.getSkillsList('https://localhost:17922/api/getSkillList', { 'X-Authorization-Firebase': nextProps.token })
+      this.props.getProfile(`https://localhost:17922/api/getProfile?identifier=${uid}&type=${type}`, { 'X-Authorization-Firebase': nextProps.token })
     }
   }
 
   removeSkill = (skill) => {
-    if(this.props.uid && this.props.token) {
-      this.props.removeSkill(`https://localhost:17922/api/removeSkill?username=${this.props.uid}&name=${skill}`, { 'X-Authorization-Firebase': this.props.token})
+    if (this.props.uid && this.props.token) {
+      this.props.removeSkill(`https://localhost:17922/api/removeSkill?username=${this.props.uid}&name=${skill}`, { 'X-Authorization-Firebase': this.props.token })
     } else {
       console.log('UID or Token does not exist!')
     }
   }
 
+  //  <NotificationPanel />
+
+  //   <UserInfo />
+
+
   render() {
     return (
       <div className="Profile">
-        <NotificationPanel />
-       
-        <UserInfo />
-
-        <div class="navbar">
-              <a href="#">Link</a>
-              <a href="#">Link</a>
-              <a href="#">Link</a>
-              <a href="#" class="right">Link</a>
-          </div>
-                    
-
-         <div className="header">
-             <h1>My Profile</h1>
-                 <p>A website created by me!</p>  
-          </div>
-
-     
-
-          <div className="row">
-            <div className="side">...</div>
-            <div className="main">...</div>
-          </div>
-
+        <div className="header">
+          <h1>Until I learn redux</h1>
+          <p>A website created by me!</p>
+        </div>
+        <div className="User">
+          <h1> How do i get the current user info from redux </h1>
+        </div>
+        <div className="Friends">
+          <h1> How do i get the current user info from redux </h1>
+        </div>
+        <div className="skillRatings">
+          <h1> How do I get the current user info from redux </h1>
+        </div>
+        <div className="sidebar">
+          <p> This is where our skills can go. </p>
           <div className="skills">
-            <div>
-               <p>
-                    <SkillsPanel />   
-               </p>
-            </div>
+            <SkillsPanel />
           </div>
-
-
+        </div>
+        <div className="">
           <footer className="footer">
-           <p>Find me on social media.</p>
-              <i className="fa fa-facebook-official w3-hover-opacity"></i>
-              <i className="fa fa-instagram w3-hover-opacity"></i>
-              <i className="fa fa-snapchat w3-hover-opacity"></i>
-              <i className="fa fa-pinterest-p w3-hover-opacity"></i>
-              <i className="fa fa-twitter w3-hover-opacity"></i>
-              <i className="fa fa-linkedin w3-hover-opacity"></i>
+            <p>Find me on social media.</p>
+            <i className="fa fa-facebook-official w3-hover-opacity"></i>
+            <i className="fa fa-instagram w3-hover-opacity"></i>
+            <i className="fa fa-snapchat w3-hover-opacity"></i>
+            <i className="fa fa-pinterest-p w3-hover-opacity"></i>
+            <i className="fa fa-twitter w3-hover-opacity"></i>
+            <i className="fa fa-linkedin w3-hover-opacity"></i>
           </footer>
+        </div>
       </div>
-
-    )
+        )
   }
 }
 function getScrollPercent() {
   return (
-    (document.documentElement.scrollTop || document.body.scrollTop) 
-    / ( (document.documentElement.scrollHeight || document.body.scrollHeight) 
-    - document.documentElement.clientHeight) 
+    (document.documentElement.scrollTop || document.body.scrollTop)
+    / ( (document.documentElement.scrollHeight || document.body.scrollHeight)
+    - document.documentElement.clientHeight)
     * 100);
 }
 
 
 const mapStateToProps = (state) => {
 	return {
-    uid: state.user.data ? state.user.data.uid : null,
+          uid: state.user.data ? state.user.data.uid : null,
     token: state.auth.token
 	}
 }
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-    getSkills: (url, headers) => dispatch(getSkills(url, headers)),
+          getSkills: (url, headers) => dispatch(getSkills(url, headers)),
     removeSkill: (url, headers) => dispatch(removeSkill(url, headers)),
     getProfile: (url, headers) => dispatch(getProfile(url, headers)),
     getSkillsList: (url, headers) => dispatch(getSkillsList(url, headers)),
