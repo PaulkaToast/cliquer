19a2ad7c91341c5535a4ddf8d631b975f46fee0f
Jordan Buckmaster <buckmast@purdue.edu>
2018-03-17 13:48:24 -0500

    Create new branch and fi issues in profile page

--- client/src/components/Profile/UserInfo.js
+++ client/src/components/Profile/UserInfo.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import '../../css/UserInfo.css'
-import jmbuck from '../../img/jmbuck.jpg'
 
 class UserInfo extends Component {
   render() {
@@ -8,7 +7,6 @@ class UserInfo extends Component {
     return (
       <div className="UserInfo">
            <div className ="img">
-                <img src={jmbuck} alt="AvatarX"></img>  
             </div> 
            <div className = "UN"> 
                    {user ? user.displayname : ''}  
