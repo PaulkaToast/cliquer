aed25481bffe6893d23ba83adb47e606913f996e
JordanR159 <22157853+JordanR159@users.noreply.github.com>
2018-03-25 20:00:02 -0400

    Changes to test case

--- server/src/test/java/com/styxxco/cliquer/tests/SprintTwoServicesTest.java
+++ server/src/test/java/com/styxxco/cliquer/tests/SprintTwoServicesTest.java
@@ -674,6 +674,7 @@ public class SprintTwoServicesTest {
         Skill newJava = skillRepository.findBySkillNameAndSkillLevel("Java", 5);
         Skill newVim = skillRepository.findBySkillNameAndSkillLevel("VIM", 7);
         kevin = accountRepository.findByAccountID(kevin.getAccountID());
+        assertEquals(2, kevin.getSkillIDs().size());
         assertEquals("Java", skillRepository.findBySkillID(kevin.getSkillIDs().get(0)).getSkillName());
         assertEquals(5, skillRepository.findBySkillID(kevin.getSkillIDs().get(0)).getSkillLevel());
         assertEquals(true, kevin.getSkillIDs().contains(newJava.getSkillID()));
