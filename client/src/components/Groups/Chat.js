924a638917b854a792ff8eb23a714647f9cf7b42
PaulkaToast <toth21@purdue.edu>
2018-04-15 22:25:29 -0400

    Add date and time stamps for chat message

--- client/src/components/Groups/Chat.js
+++ client/src/components/Groups/Chat.js
@@ -7,13 +7,22 @@ import '../../css/Chat.css'
 import { getChatLog, postChatMessage, updateChatLog } from '../../redux/actions'
 import url from '../../server'
 
-const Message = ({message, sender, align}) => {
+const Message = ({message, sender, align, time}) => {
   if (!message) return <div></div>;
+  var time1 = ""
+  var time2 = ""
+  if (align == "sender-message-right"){
+    time1 = time
+  }else{
+    time2 = time
+  }
   return (
     <div className={align}>
       <Badge className={align.concat("-badge")}>{sender}</Badge>
       <br/>
+      <span className="time-stamp-right">{time1}</span>
       <Alert className="single-message" className={align.concat("-alert")}> {message} </Alert>
+      <span className="time-stamp-left">{time2}</span>
     </div>);
 };
 
@@ -65,12 +74,50 @@ class Chat extends Component {
     } else {
       if (data[0]){
         this.state.messages = data.map( (m) => {
-          return {sender: m.senderName, message: m.content, id: m.senderID}
+          var hour = ""
+          var minute = ""
+          var time = ""
+          var ampm = ""
+          if(m.creationTime["hour"] > 12){
+            hour = (m.creationTime["hour"] - 12) + ":" 
+            ampm = " PM"
+          }else{
+            hour = m.creationTime["hour"] + ":"
+            ampm = " AM"
+          }
+          if(Math.floor(m.creationTime["minute"] / 10) == 0){
+            minute = "0" + m.creationTime["minute"]
+          }else{
+            minute = m.creationTime["minute"]
+          }
+          time = hour + minute + ampm
+          return {sender: m.senderName, message: m.content, id: m.senderID, 
+            date: m.creationDate["dayOfWeek"] + " " + m.creationDate["month"] + 
+            " " + m.creationDate["dayOfMonth"], time: time}
         })
         this.setState(this.state)
       } else {
+          var hour = ""
+          var minute = ""
+          var time = ""
+          var ampm = ""
+          if(data.creationTime["hour"] > 12){
+            hour = (data.creationTime["hour"] - 12) + ":" 
+            ampm = " PM"
+          }else{
+            hour = data.creationTime["hour"] + ":"
+            ampm = " AM"
+          }
+          if(Math.floor(data.creationTime["minute"] / 10) == 0){
+            minute = "0" + data.creationTime["minute"]
+          }else{
+            minute = data.creationTime["minute"]
+          }
+          time = hour + minute + ampm
         this.state.messages.push({
-          sender: data.senderName, message: data.content, id: data.senderID
+          sender: data.senderName, message: data.content, id: data.senderID, 
+          date: data.creationDate["dayOfWeek"] + " " + data.creationDate["month"] + 
+          " " + data.creationDate["dayOfMonth"], time: time
         })
         this.setState(this.state)
       }
@@ -114,16 +161,22 @@ class Chat extends Component {
 
   render() {
     const messages = this.state.messages;
+    var passingDate = "";
 
     return (
       <div className="Chat">
         <div className="message-container">
           {
-            messages.map((c, index) => { 
+            messages.map((c, index) => {
+              var dateDiv = <div></div>
+              if( c.date != passingDate){
+                var dateDiv = <div className="date-badge-center"><Badge>{c.date}</Badge></div>
+                passingDate = c.date
+              } 
               if( c.id === this.props.user.uid ){
-                return <Message align="sender-message-right" key={index} sender={c.sender} message={c.message}></Message>
+                return <div>{dateDiv}<Message align="sender-message-right" key={index} sender={c.sender} message={c.message} time={c.time}></Message></div>
               } else {
-                return <Message align="sender-message-left" key={index} sender={c.sender} message={c.message}></Message> 
+                return <div>{dateDiv}<Message align="sender-message-left" key={index} sender={c.sender} message={c.message} time={c.time}></Message></div>
               }
             })
           }
