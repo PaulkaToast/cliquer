090c49026a65789772103f2d7ef6ff12f52128c9
PaulkaToast <toth21@purdue.edu>
2018-04-21 01:16:27 -0400

    Delete notifcation without re-rendering

--- client/src/components/Profile/NotificationPanel.js
+++ client/src/components/Profile/NotificationPanel.js
@@ -6,10 +6,37 @@ import Notification from './Notification'
 import { getMessages } from '../../redux/actions'
 
 class NotificationPanel extends Component {
+  constructor(props){
+    super(props);
 
+    this.state = {
+      notifications: {},
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      notifications: this.props.notifications
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(this.props != nextProps) {
+      this.setState({
+        notifications: nextProps.notifications
+      });
+    }
+  }
+
+  deleteAndRender(messageId) {
+    this.props.deleteNotification(messageId);
+    var newState = this.state;
+    delete newState.notifications[messageId]
+    this.setState(newState);
+  }
 
   render() {
-    const { notifications } = this.props
+    const notifications = this.state.notifications
     return (
       <div className="NotificationPanel">
         <h4 className="notification-header"> Notifications </h4>
@@ -20,7 +47,7 @@ class NotificationPanel extends Component {
                 return <Notification
                   notification={notifications[key]}
                   i={i}
-                  deleteNotification={this.props.deleteNotification}
+                  deleteNotification={this.deleteAndRender.bind(this)}
                   acceptNotification={this.props.acceptNotification}
                   rejectNotification={this.props.rejectNotification}
                   markAsRead={this.props.markAsRead}
